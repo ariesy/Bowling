2ed16f2e1dfd4c374f98e95558612d70ffbbd83d
Sirius <ariesyzhk@gmail.com>
2013-11-05 23:12:46 +0800

    push

---
+++ Sirius.Bowling.UI.Wpf/ViewModels/FrameScoreBoard.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sirius.Bowling.UI.Wpf.ViewModels
+{
+    public class FrameScoreBoard : NotificationObject
+    {
+        public string Score1 { get; set; }
+
+        public string Score2 { get; set; }
+    }
+}
